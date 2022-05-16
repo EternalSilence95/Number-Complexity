e50c98849ba84d39e150d7afd60522b526e114ec
Sargis <82839525+Eternalrain95@users.noreply.github.com>
2022-05-16 21:55:48 +0400

    Add files via upload

---
+++ Number Complexity Range.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 12 16:02:07 2022
+
+@author: saqo2
+"""
+
+from primePy import primes
+import math
+from pylab import *
+n = int(input('Enter some natural number '))
+range = int(input('Enter the range '))
+l = n + range
+xresult = []
+yresult = []
+def NC(n, range):
+    while n < l:
+        i = primes.factors(n)
+        j = []
+        while len(i) > 0:
+            k = 1/i.pop()
+            j.append(k)
+        a = round(n * sum(j))
+        print(n,' has Complexity ',a)
+        xresult.append(n)
+        yresult.append(a)
+        n = n + 1
+NC(n, range)
+plot(xresult, yresult, marker='o')
+show()     
+    
+    
+
+        
+         
+        
+
+        
