ac33e1017fb1002e5317d7b9c291bdd3458fb08a
Sargis <82839525+Eternalrain95@users.noreply.github.com>
2022-05-16 22:09:46 +0400

    Add files via upload

---
+++ Number Relative Complexity Range.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 16 22:03:08 2022
+
+@author: saqo2
+"""
+
+from primePy import primes
+import math
+from pylab import *
+n = int(input('Enter some natural number '))
+range = int(input('Enter the range '))
+l = n + range
+xresult = []
+yresult = []
+def NC(n, range):
+    while n < l:
+        i = primes.factors(n)
+        j = []
+        while len(i) > 0:
+            k = 1/i.pop()
+            j.append(k)
+        print(n,' has relative Complexity ',sum(j))
+        xresult.append(n)
+        yresult.append(sum(j))
+        n = n + 1
+NC(n, range)
+plot(xresult, yresult, marker='o')
+show()     
