32dec3d2f49dcf28fd25fe7b27c5b969427e7c59
Sargis <82839525+Eternalrain95@users.noreply.github.com>
2022-06-09 22:07:27 +0400

    Add files via upload

---
+++ Dynamic In Range Natural Log.py
@@ -0,0 +1,56 @@
+
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 12 16:02:07 2022
+
+@author: saqo2
+"""
+
+from primePy import primes
+import math
+from pylab import *
+n = int(input('Enter some natural number '))
+n1 = n
+iter = int(input('Enter number of itrations '))
+r = int(input('Enter range '))
+def NC(n, iter):
+    global xresult, yresult
+    xresult = [0]
+    yresult = [math.log(n)]
+    if(iter == 0):
+        print(n)
+    else:
+        print()
+        print('First number is ',n)
+        print('ln ',n,' is ',math.log(n))
+        print()
+        m = 0
+        while m < iter:
+            i = primes.factors(n)
+            print('factors of ',n,' are ', i)
+            j = []
+            while len(i) > 0:
+                k = 1/i.pop()
+                j.append(k)
+            a = round(n * sum(j))
+            print('Relative complexity is ',sum(j))
+            print('Complexity is ',a)
+            print('ln complexity is ', math.log(a))
+            print()
+            n = a
+            m = m + 1
+            xresult.append(m)
+            yresult.append(math.log(a))
+xlabel('Time')
+ylabel('Value')            
+for n in range(n1, n1+r, 1):
+    NC(n, iter)
+    plot(xresult, yresult, marker='o')        
+show()    
+    
+
+        
+         
+        
+
+        
