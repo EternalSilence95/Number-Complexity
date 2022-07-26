b097e5d3f5b2490025904a88ad4ed13a2b6e8eb1
Sargis <82839525+Eternalrain95@users.noreply.github.com>
2022-07-26 16:05:52 +0400

    Update Number_complexity_Dynamics.py

--- Number_complexity_Dynamics.py
+++ Number_complexity_Dynamics.py
@@ -1,23 +1,26 @@
 from primePy import primes
 import math
+from fractions import Fraction
 n = int(input('Enter some natural number '))
 iter = int(input('Enter number of itrations '))
 if iter == 0:
     print(n)
 else:
+    print()
     print('First number is ',n)
+    print()
     m = 0
     while m < iter:
         i = primes.factors(n)
         print('factors are ', i) 
         j = []
         while len(i) > 0:
-            k = 1/i.pop()
+            k = Fraction(1, i.pop())
             j.append(k)
         a = round(n * sum(j))
-        print('Relative complexity is ',sum(j))
-        print()
+        print('Relative complexity is ',sum(j), '=', float(sum(j)))
         print('Complexity is ',a)
+        print()
         n = a
         m = m + 1
         
