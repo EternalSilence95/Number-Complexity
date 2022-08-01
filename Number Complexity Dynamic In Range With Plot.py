317c11de4ce0bf294677bc69fa51d0f5cc9b4990
Sargis <82839525+Eternalrain95@users.noreply.github.com>
2022-08-01 17:02:20 +0400

    Update Number Complexity Dynamic In Range With Plot.py

--- Number Complexity Dynamic In Range With Plot.py
+++ Number Complexity Dynamic In Range With Plot.py
@@ -10,43 +10,52 @@ from primePy import primes
 import math
 from pylab import *
 from fractions import Fraction
-n = int(input('Enter some natural number '))
-n1 = n
-iter = int(input('Enter number of itrations '))
-r = int(input('Enter range '))
-figure(dpi=1200)
-def NC(n, iter):
-    global xresult, yresult
-    xresult = [0]
-    yresult = [n]
-    if(iter == 0):
-        print(n)
-    else:
-        print()
-        print('First number is ',n)
-        print()
-        m = 0
-        while m < iter:
-            i = primes.factors(n)
-            print('factors of ',n,' are ', i)
-            j = []
-            while len(i) > 0:
-                k = Fraction(1, i.pop())
-                j.append(k)
-            a = int(n * sum(j))
-            print('Relative complexity is ',sum(j), '=', float(sum(j)))
-            print('Complexity is ',a)
-            print()
-            n = a
-            m = m + 1
-            xresult.append(m)
-            yresult.append(a)
-xlabel('Time')
-ylabel('Value')            
-for n in range(n1, n1+r, 1):
-    NC(n, iter)
-    plot(xresult, yresult, marker='o')        
-show()    
+while True:
+   n = input('Enter some natural number ')
+   if n == 'stop': break
+   n1 = n
+   iter = input('Enter number of itrations ')
+   r = input('Enter range ')
+   # figure(dpi=1200)
+   def NC(n, iter):
+       n2 = int(n)
+       i1 = int(iter)
+       global xresult, yresult
+       xresult = [0]
+       yresult = [n]
+       if(iter == 0):
+           print(n2)
+       else:
+           print()
+           print('First number is ',n2)
+           print()
+           m = 0
+           while m < i1:
+               i = primes.factors(n2)
+               print('factors of ',n2,' are ', i)
+               j = []
+               while len(i) > 0:
+                   k = Fraction(1, i.pop())
+                   j.append(k)
+               a = int(n2 * sum(j))
+               print('Relative complexity is ',sum(j), '=', float(sum(j)))
+               print('Complexity is ',a)
+               print()
+               n2 = a
+               m = m + 1
+               xresult.append(m)
+               yresult.append(a)
+   xlabel('Time')
+   ylabel('Value')   
+   try:      
+      for n in range(int(n1), int(n1)+int(r), 1):
+          NC(n, iter)
+          plot(xresult, yresult, marker='o')
+      show()
+   except:
+      print('Bad input, try again')   
+print('Have a good day')      
+    
     
 
         
