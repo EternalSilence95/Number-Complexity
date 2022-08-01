7ad4bac1172165cb271f64124de858a307eb067a
Sargis <82839525+Eternalrain95@users.noreply.github.com>
2022-08-01 16:58:57 +0400

    Update Number_complexity_Dynamics.py

--- Number_complexity_Dynamics.py
+++ Number_complexity_Dynamics.py
@@ -1,31 +1,37 @@
 from primePy import primes
 import math
 from fractions import Fraction
-n = int(input('Enter some natural number '))
-iter = int(input('Enter number of itrations '))
-if iter == 0:
-    print(n)
-else:
-    print()
-    print('First number is ',n)
-    print()
-    m = 0
-    while m < iter:
-        i = primes.factors(n)
-        print('factors of ', n, 'are ', i) 
-        j = []
-        while len(i) > 0:
-            k = Fraction(1, i.pop())
-            j.append(k)
-        a = round(n * sum(j))
-        print('Relative complexity is ',sum(j), '=', float(sum(j)))
-        print('Complexity is ',a)
-        print()
-        n = a
-        m = m + 1
-        
+while True:
+   n = input('Enter some natural number ')
+   if n == 'stop': break
+   iter = input('Enter number of itrations ')
+   try:
+      v = int(n)
+      s = int(iter)
+      if s == 0:
+          print(n)
+      else:
+          print()
+          print('First number is ',n)
+          print()
+          m = 0
+          while m < s:
+              i = primes.factors(v)
+              print('factors of ',v, 'are ', i) 
+              j = []
+              while len(i) > 0:
+                  k = Fraction(1, i.pop())
+                  j.append(k)
+              a = int(v * sum(j))
+              print('Relative complexity is ',sum(j), '=', float(sum(j)))
+              print('Complexity is ',a)
+              print()
+              v = a
+              m = m + 1 
+   except:
+      print('Bad input, try again') 
+print('Have a good day')        
          
         
 
         
-
